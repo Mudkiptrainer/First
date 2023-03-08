5de43970eaffd195d48dd832627ef18731f2d4f1
Harley Collins <hcollins@heidelberg.edu>
2023-03-08 15:43:51 -0500

    all

--- stuff/IO.py
+++ stuff/IO.py
@@ -1,3 +1,6 @@
-print("here you can view the prices and quantity of items we have.")
-import inven
-print(inven.snacks)
+
+print("Welcome to our Vending Machine!")
+print("Have a look around, see what you would like")
+print("We have ChocoRamo's for $2.00\n Hersheys for $1.50\n Snickers for $1.50\n Twix for $1.75\n Crunch bars for $2.20\n Chocobreak for $1.60\n Chocolate milk for $3.20\n and some Jr,Double,Tripple Whoppers for $6.50")
+
+import main
